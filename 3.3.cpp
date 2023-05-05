717966c6f2bf3a7db9f976d9cf460edbdc11a53e
leonooo13 <lidefree@qq.com>
2023-05-05 20:42:02 +0800

    3.3

---
+++ 3.3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#define Max 20
+bool isSymmetric(int matrix[][Max], int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            if (matrix[i][j] != matrix[j][i]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
